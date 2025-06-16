29ca46908c30e6933099831ccf9dd62e6d706f4a
Eduardo Mendes <edumendes@gmail.com>
2025-06-16 13:16:00 -0400

    renaming files

---
+++ simple_langchain.py
@@ -0,0 +1,31 @@
+from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+number_of_days = 7
+number_of_children = 2
+activity = "beach"
+
+template = PromptTemplate.from_template(
+    "Create a {number_of_days}-day travel itinerary for a family with {number_of_children} children who like {activity}"
+)
+
+prompt = template.format(
+    number_of_days=number_of_days,
+    number_of_children=number_of_children,
+    activity=activity
+)
+
+print(prompt)
+
+llm = ChatOpenAI(
+    model="gpt-3.5-turbo",
+    temperature=0.7,
+    openai_api_key=os.getenv("OPENAI_API_KEY")
+)
+
+response = llm.invoke(prompt)
+print(response.content)
