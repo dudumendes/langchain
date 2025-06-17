fe83ed592cddfdb1c486bb602600c3d0093e917c
Eduardo Mendes <edumendes@gmail.com>
2025-06-17 17:42:28 -0400

    text file processing example

---
+++ retrieval_langchain.py
@@ -0,0 +1,38 @@
+from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationSummaryMemory
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_openai  import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain.chains import RetrievalQA
+
+from langchain.globals import set_debug
+import os
+from dotenv import load_dotenv
+
+set_debug(True)
+load_dotenv()
+
+llm = ChatOpenAI(
+    model="gpt-3.5-turbo",
+    temperature=0.7,
+    openai_api_key=os.getenv("OPENAI_API_KEY")
+)
+
+loaded_text = TextLoader("mc-gold.txt", encoding="utf-8")
+documents = loaded_text.load()
+
+splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+texts = splitter.split_documents(documents)
+
+embeddings = OpenAIEmbeddings()
+db = FAISS.from_documents(texts, embeddings)
+
+qa_chain = RetrievalQA.from_chain_type(llm, retriever=db.as_retriever())
+
+question = "Como devo proceder caso tenho um item comprado roubado?"
+result = qa_chain.invoke({"query" : question})
+print(result)
