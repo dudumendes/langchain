74908b9082c6f5661257273ca26f24c941c9e5cc
Eduardo Mendes <edumendes@gmail.com>
2025-06-17 16:48:44 -0400

    summary example

---
+++ summary_langchain.py
@@ -0,0 +1,39 @@
+from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationSummaryMemory
+from langchain.globals import set_debug
+import os
+from dotenv import load_dotenv
+
+set_debug(True)
+load_dotenv()
+
+llm = ChatOpenAI(
+    model="gpt-3.5-turbo",
+    temperature=0.7,
+    openai_api_key=os.getenv("OPENAI_API_KEY")
+)
+
+messages = [
+        "Quero visitar um lugar no Brasil famoso por suas praias e cultura. Pode me recomendar?",
+        "Qual é o melhor período do ano para visitar em termos de clima?",
+        "Quais tipos de atividades ao ar livre estão disponíveis?",
+        "Alguma sugestão de acomodação eco-friendly por lá?",
+        "Cite outras 20 cidades com características semelhantes às que descrevemos até agora. Rankeie por mais interessante, incluindo no meio a que você já sugeriu.",
+        "Na primeira cidade que você sugeriu lá atrás, quero saber 5 restaurantes para visitar. Responda somente o nome da cidade e o nome dos restaurantes.",
+]
+memory = ConversationSummaryMemory(llm=llm)
+
+
+conversation = ConversationChain(
+    llm=llm, verbose=True, memory=memory)
+
+talking = ""
+
+for message in messages:
+    result = conversation.predict(input=message)
+    print(result)
+
+print(memory.load_memory_variables({}))
